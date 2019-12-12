411eeccb9fc3fee403f6859feff7d2549c5014a0
thevenine <seyeoyeniran2u@gmail.com>
2019-12-12 23:03:16 +0100

    fix home

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,5 @@
-const handleHome = (req, res, db) => {
-    db('users').select('name', 'email', 'entries')
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json('no users found');
-        })
+const handleHome = (req, res) => {
+   res.json('it is working');
 }
 
 module.exports = {
