2889593506f053ba4eb7a9f2868e19bcb76041c0
thevenine <seyeoyeniran2u@gmail.com>
2019-12-13 01:00:30 +0100

    update env variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,19 +1,19 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: '6ede32fb1a4040aa9509839c2e29e2b8'
-  })
+    apiKey: process.env.API_CLARIFAI
+})
 
-  const handleApiCall = (req, res) => {
-    const {input} = req.body;
+const handleApiCall = (req, res) => {
+    const { input } = req.body;
     app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, input)
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+        .predict(Clarifai.FACE_DETECT_MODEL, input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
 
-  }
+}
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
